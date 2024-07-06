a8853bbdfeceae8bc41642334e3d4c2c5e940a97
Alvaro <alvarolukmanto@gmail.com>
2024-07-06 21:18:54 +0800

    feature - generate calendar

--- src/components/DateRangePicker/DateRangePicker.jsx
+++ src/components/DateRangePicker/DateRangePicker.jsx
@@ -1,30 +1,88 @@
+import { useState } from 'react'
 import styles from './DateRangePicker.module.scss'
 import DayPickerButton from './DayPickerButton'
+import { useEffect } from 'react'
 
 function DateRangePicker() {
+  const [todayTimestamp] = useState(() => {
+    return Date.now()
+  })
+  const [selectedYear] = useState(() => {
+    const date = new Date(todayTimestamp)
+    return date.getFullYear()
+  })
+  const [selectedMonthIndex] = useState(() => {
+    const date = new Date(todayTimestamp)
+    return date.getMonth()
+  })
+  const [headerTitle, setHeaderTitle] = useState('')
+  const [calendar, setCalendar] = useState([])
+
+  useEffect(() => {
+    setHeaderTitle(`${selectedYear}年 ${selectedMonthIndex + 1}月`)
+  }, [selectedMonthIndex, selectedYear])
+
+  useEffect(() => {
+    const currentCalendar = generateCalendar(selectedYear, selectedMonthIndex)
+    setCalendar(currentCalendar)
+  }, [selectedMonthIndex, selectedYear])
+
+  function generateCalendar(selectedYear, selectedMonthIndex) {
+    const calendar = []
+    const lastDateObj = new Date(selectedYear, selectedMonthIndex + 1, 0)
+    const lastDate = lastDateObj.getDate()
+    const lastDatePrevMonth = new Date(
+      selectedYear,
+      selectedMonthIndex - 1,
+      0
+    ).getDate()
+    const firstWeekdayIndex = new Date(
+      selectedYear,
+      selectedMonthIndex,
+      1
+    ).getDay()
+    const lastWeekdayIndex = lastDateObj.getDay()
+
+    // populate previous month days
+    for (let i = 0; i < firstWeekdayIndex; i++) {
+      const date = lastDatePrevMonth - (firstWeekdayIndex - 1 - i)
+      // TODO store Date obj here
+      calendar.push({ date, transparent: true })
+    }
+
+    // current month dates
+    for (let i = 1; i <= lastDate; i++) {
+      // TODO store Date obj here
+      calendar.push({ date: i, transparent: false })
+    }
+
+    // next month dates
+    for (let i = 1; i < 7 - lastWeekdayIndex; i++) {
+      // TODO store Date obj here
+      calendar.push({ date: i, transparent: true })
+    }
+
+    return calendar
+  }
+
+  const pickerItems = calendar.map((item, index) => {
+    return (
+      <DayPickerButton key={item.day + index} transparent={item.transparent}>
+        {item.date}日
+      </DayPickerButton>
+    )
+  })
+
   return (
     <div className={styles['date-range-picker']}>
       <div className={styles.header}>
         <button className={styles['btn-prev-month']}>&#60;</button>
-        <div className={styles['selected-month']}>2022年 7月</div>
+        <div className={styles['selected-month']}>{headerTitle}</div>
         <button className={styles['btn-next-month']}>&#62;</button>
       </div>
 
-      <div className={styles['day-picker']}>
-        <DayPickerButton transparent>29日</DayPickerButton>
-
-        <DayPickerButton transparent>30日</DayPickerButton>
-
-        <DayPickerButton transparent>31日</DayPickerButton>
-
-        <DayPickerButton today>1日</DayPickerButton>
-
-        <DayPickerButton active>2日</DayPickerButton>
-
-        <DayPickerButton active>3日</DayPickerButton>
-
-        <DayPickerButton>4日</DayPickerButton>
-      </div>
+      {/* Most left is sunday */}
+      <div className={styles['day-picker']}>{pickerItems}</div>
     </div>
   )
 }
